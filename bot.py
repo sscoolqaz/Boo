f0281082a7e8bd86eb1ff28b6a3cd3b23c74685a
Martin <martinwjwilson@gmail.com>
2019-02-13 00:21:30 +0000

    tutorial 3

--- bot.py
+++ bot.py
@@ -3,16 +3,22 @@ from discord.ext import commands
 
 TOKEN = "NTQ1MDExNDU0MTYwNjY2NjM0.D0Tc_g.y-9swzzfUUwueJQVZnRSosC7ZhQ"
 
-client = commands.Bot(command_prefix = ".")
+client = commands.Bot(command_prefix = ">")
 
 @client.event
 async def on_ready():
     print("Bot is ready...")
 
-@client.event
-async def on_message(message):
-    author = message.author
-    content = message.content
-    print(`{}: {}`.format(author, content))
+@client.command()
+async def ping():
+    await client.say("Pong!")
+
+@client.command()
+async def echo(*args):
+    output = ""
+    for word in args:
+        output += word
+        output += " "
+    await client.say(output)
 
 client.run(TOKEN)
