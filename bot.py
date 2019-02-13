1fe9eb8618f3c976aeffc3bed5c4206ef5915534
Martin <martinwjwilson@gmail.com>
2019-02-13 02:03:29 +0000

    added some more features

--- bot.py
+++ bot.py
@@ -9,12 +9,27 @@ client = commands.Bot(command_prefix = ">")
 async def on_ready():
     print("Bot is ready...")
 
+@client.event
+async def on_message(message):
+    if (message.author.bot == True):
+        print("The user was a bot")
+        return
+    if (message.author.id == "276883477318860800"):
+        await client.send_message(message.channel, "i see you Aimee OwO")
+        return
+    if (message.author.id == "247810647667113996"):
+        await client.send_message(message.channel, "https://www.skiddle.com/artists/basshunter-123458771/")
+        return
+    print("A user has send a message")
+    await client.process_commands(message)
+
 @client.command()
 async def ping():
     await client.say("Pong!")
 
-@client.command()
-async def echo(*args):
+@client.command(pass_context = True)
+async def echo(msg, *args):
+    await client.delete_message(msg.message)
     output = ""
     for word in args:
         output += word
