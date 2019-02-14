4e38af250f1945582a660794f692869234c50e15
Martin <martinwjwilson@gmail.com>
2019-02-14 00:32:33 +0000

    finished for the day

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 
 TOKEN = "NTQ1MDExNDU0MTYwNjY2NjM0.D0Tc_g.y-9swzzfUUwueJQVZnRSosC7ZhQ"
 
@@ -17,15 +18,37 @@ async def on_message(message):
     if (message.author.id == "276883477318860800"):
         await client.send_message(message.channel, "i see you Aimee OwO")
         return
-    if (message.author.id == "247810647667113996"):
-        await client.send_message(message.channel, "https://www.skiddle.com/artists/basshunter-123458771/")
-        return
-    print("A user has send a message")
+    # if (message.author.id == "247810647667113996"):
+    #     await client.send_message(message.channel, "https://www.skiddle.com/artists/basshunter-123458771/")
+    #     return
+
+    # random switch statement
+    ranNo = random.randint(1,4)
+    if ranNo == 1:
+        await client.send_message(message.channel, "Case 1")
+    elif ranNo == 2:
+        await client.send_message(message.channel, "Case 2")
+    elif ranNo == 3:
+        await client.send_message(message.channel, "Case 3")
+    elif ranNo == 4:
+        await client.send_message(message.channel, "Case 4")
+
+    # check if a command should also be executed as on_message has priority
     await client.process_commands(message)
 
 @client.command()
 async def ping():
     await client.say("Pong!")
+
+# @client.command()
+# async def randomMessage(args):
+#     print(args[1])
+#     switcher = {
+#         1: "Hi 1",
+#         2: "Hii 2",
+#         3: "Hiii 3"
+#     }
+#     print switcher.get(args[1], "Invalid month")
 
 @client.command(pass_context = True)
 async def echo(msg, *args):
