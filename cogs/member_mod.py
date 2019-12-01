7d3cf01f68c78f8598f0914be76083e6c9360c2e
sscoolqaz <seth.spears99@gmail.com>
2019-12-01 15:19:55 -0600

    Implemented early ver of records
    Consolidated the jsons into one, if need be we can add some more elements/go more indepth.
    Created base record, createRecord()

--- cogs/member_mod.py
+++ cogs/member_mod.py
@@ -12,11 +12,85 @@ class member_moderation(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # conversion to seconds for use with unix epoch ie time()
+    def convtime(self, toconv):
+    numstr = ""
+    for i in range(0, len(toconv)):
+        if toconv[i].isdigit():
+            numstr = numstr + toconv[i]
+    if toconv.endswith("s"): # seconds
+        num = int(numstr)
+    elif toconv.endswith("m"): # minutes
+        num = int(numstr) * 60
+    elif toconv.endswith("h"): # hours
+        num = int(numstr) * (60^2)
+    elif toconv.endswith("d"): # days
+        num = int(numstr) * 24 * (60^2)
+    elif toconv.endswith("w"): # weeks
+        num = int(numstr) * 7 * 24 * (60^2)
+    elif toconv.endswith("M"): # months
+        num = int(numstr) * 30 * 24 * (60^2)
+    elif toconv.endswith("y"): # years
+        num = int(numstr) * 365 * 24 * (60^2)
+    return num
+    
+    # used to create the base record
+    def createRecord(self, ctx, member, isMuted=0, timer=0, muteReason='None', warnings=0):
+        with open('records.json') as f:
+            data = json.load(f)
+        data.append( {  'id': member.id,
+                        'name': str(member.display_name),
+                        'isMuted': isMuted,
+                        'timer': timer,
+                        'muteReason': muteReason,
+                        'warnings': warnings} )
+        with open('records.json', 'w') as f:
+            json.dump(data, f, indent = 2)
+        with open('records.json') as f:
+            data = json.load(f)
+            return data
 
-    # logging.basicConfig(level=logging.DEBUG)
-
-
-    # mute all users -not finished-
+    # DO NOT USE UNLESS FILLING RECORDS FOR FIRST TIME
+    # IT WILL CREATE DUPLICATES IF YOU DO
+    @commands.command(hidden = True)
+    @commands.has_role(config.role_dict.get("red_panda"))
+    async def addalltorecords(self, ctx):
+        gmem = ctx.guild.members
+        async with ctx.channel.typing():
+            for member in gmem:
+                self.createRecord(ctx, member)
+            await ctx.send("done")
+    
+    @commands.command(hidden = True)
+    @commands.has_role(config.role_dict.get("admin"))
+    async def record(self, ctx, message=None):
+        if ctx.message.channel.id == config.channel_dict.get("adminchannel"):
+            if message is not None:
+                member = ctx.message.mentions[0]
+            if message is None:
+                member = ctx.message.author
+            record_exists = False
+            with open('records.json') as f:
+                data = json.load(f)
+            for user in data:
+                if user['id'] == member.id:
+                    embed = discord.Embed(title=f"{member.display_name}'s Record", description=f"{member.mention}")
+                    embed.set_thumbnail(url=member.avatar_url)
+                    if user['isMuted'] == 1:
+                        embed.add_field(name="isMuted:", value=user['isMuted'], inline=False)
+                        embed.add_field(name="Time Left:", value=f"{user['timer']-time():.2f} seconds remaining", inline=False)
+                        embed.add_field(name="muteReason:", value=f"{user['muteReason']}")
+                    embed.add_field(name="Warnings:", value=user['warnings'], inline=False)
+                    record_exists = True
+            if record_exists != True:
+                data = self.createRecord(ctx, member)
+                embed = discord.Embed(title=f"{member.display_name}'s Record", description=f"{member.mention}")
+                embed.set_thumbnail(url=member.avatar_url)
+                embed.add_field(name="isMuted:", value=user['isMuted'], inline=False)
+                embed.add_field(name="Warnings:", value=user['warnings'], inline=False)
+    
+    
+    # verify command
     @commands.command(hidden = True)
     async def verify(self, ctx):
         if ctx.message.channel.id == config.channel_dict["verify"]:
@@ -29,17 +103,41 @@ class member_moderation(commands.Cog):
             rmv_role = discord.utils.get(ctx.guild.roles, name = "Temp")
             await user.remove_roles(rmv_role)
             print(f"The role {rmv_role} was removed from {user}")
+            with open("records.json") as f:
+                data = json.load(f)
+            if user.id not in data: #checks if user is in database
+                self.createRecord(ctx, user)
+                print(f"{user} was successfully added to our records.")
+            # Server greeting when user verifies
             await entrance.send(f"<@{user.id}> just joined the server!")
 
 
     # mute specific user
     @commands.command(hidden = True)
     @commands.has_role(config.role_dict.get("admin"))
-    async def mute(self, ctx, mute_user):
+    async def mute(self, ctx, mute_user, length="24h", *, reason="None Provided."):
+        lenginsec = self.convtime(length)
         user_to_mute = ctx.message.mentions[0] # id of person to mute that was mentioned in command
-        mute_role = discord.utils.get(ctx.guild.roles, name = "Muted") # get the role to apply
+        mute_role = discord.utils.get(ctx.guild.roles, id = config.role_dict.get("Muted")) # get the role to apply
+        record_exists = False
+        # load the json file
+        with open('records.json') as f:
+            data = json.load(f)
+        # search for the warned user
+        for user in data:
+            if user['id'] == user_to_mute.id: # if the user id sent matches the one in the json file
+                user['isMuted'] = 1
+                user['timer'] = time() + lenginsec
+                user['muteReason'] = reason
+                record_exists = True # record has been found
+        # if no record exists for the mentioned user
+        if record_exists != True:
+            data = self.createRecord(ctx, user_to_mute, isMuted=1, timer=time() + lenginsec, muteReason=reason)
+        # write the edited dict to the json file
+        with open('records.json', 'w') as f:
+            json.dump(data, f, indent = 2)
         await user_to_mute.add_roles(mute_role) # assign the role
-        await ctx.send(f"{ctx.message.author} has muted {user_to_mute}")
+        await ctx.send(f"{ctx.message.author} has muted {user_to_mute} for {length}")
 
 
     # unmute specific user
@@ -47,19 +145,33 @@ class member_moderation(commands.Cog):
     @commands.has_role(config.role_dict.get("admin"))
     async def unmute(self, ctx, unmute_user):
         user_to_unmute = ctx.message.mentions[0] # id of person to unmute that was mentioned in command
-        unmute_role = discord.utils.get(ctx.guild.roles, name = "Muted") # get the role to apply
-        await user_to_unmute.remove_roles(unmute_role) # assign the role
+        unmute_role = discord.utils.get(ctx.guild.roles, id = config.role_dict.get("Muted")) # get the role to apply
+        record_exists = False
+        # load the json file
+        with open('records.json') as f:
+            data = json.load(f)
+        for user in data:
+            if user['id'] == user_to_unmute.id: # if the user id sent matches the one in the json file
+                user['timer'] = 0
+                user['isMuted'] = 0
+                user['muteReason'] = "None"
+                record_exists = True # record has been found
+        # if no record exists for the mentioned user
+        if record_exists != True:
+            data = self.createRecord(ctx, user_to_unmute)
+        with open('records.json', 'w') as f:
+            json.dump(data, f, indent = 2)
+        await user_to_unmute.remove_roles(unmute_role) # remove the role
         await ctx.send(f"{ctx.message.author} has unmuted {user_to_unmute}")
 
 
     # warn specific user
     @commands.command(hidden = True)
     @commands.has_role(config.role_dict.get("admin"))
-    async def warn(self, ctx, mute_user):
-        warning_name    = ctx.message.mentions[0].display_name # name of person to that was warned
+    async def warn(self, ctx, warneduser):
         warning_id      = ctx.message.mentions[0].id # id of person to that was warned
         # load the json file
-        with open('warnings.json') as f:
+        with open('records.json') as f:
             data = json.load(f)
         record_exists = False
         # search for the warned user
@@ -67,38 +179,36 @@ class member_moderation(commands.Cog):
             if user['id'] == warning_id: # if the warning id sent matches the one in the json file
                 # increase the users warning count
                 user['warnings'] += 1
-                print(f"{user['name']} now has {user['warnings']} warnings.") # console
                 await ctx.send(f"{user['name']} now has {user['warnings']} warnings.") # discord
                 record_exists = True # record has been found
         # if no record exists for the mentioned user
-        if record_exists == False:
-            data.append( {'id': warning_id, 'name': str(warning_name), 'warnings': 1} )
+        if record_exists != True:
+            data = self.createRecord(ctx, ctx.message.mentions[0], warnings=1)
+            await ctx.send(f"{user['name']} now has {user['warnings']} warnings.") # discord
         # write the edited dict to the json file
-        with open('warnings.json', 'w') as f:
+        with open('records.json', 'w') as f:
             json.dump(data, f, indent = 2)
 
 
     # remove warning from specific user
     @commands.command(hidden = True)
     @commands.has_role(config.role_dict.get("admin"))
-    async def unwarn(self, ctx, mute_user):
+    async def unwarn(self, ctx, warneduser):
         warning_id      = ctx.message.mentions[0].id # id of person to that was warned
         # load the json file
-        with open('warnings.json') as f:
+        with open('records.json') as f:
             data = json.load(f)
         # search for the warned user
         for user in data:
             if user['id'] == warning_id: # if the warning id sent mathes the one in the json file
                 if user['warnings'] == 0:
-                    print(f"{user['name']} doesnt have any warnings!") # console
                     await ctx.send(f"{user['name']} doesnt have any warnings!") # discord
                     continue
                 # decrements the users warning count
                 user['warnings'] -= 1
-                print(f"{user['name']} now has {user['warnings']} warnings.") # console
                 await ctx.send(f"{user['name']} now has {user['warnings']} warnings.") # discord
         # write the edited dict to the json file
-        with open('warnings.json', 'w') as f:
+        with open('records.json', 'w') as f:
             json.dump(data, f, indent = 2)
 
 
