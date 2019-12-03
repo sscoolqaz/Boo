10bd0cdb53bd69301a26d047e75c7be3c4d93b7c
sscoolqaz <seth.spears99@gmail.com>
2019-12-03 10:15:12 -0600

    typo fix + actually able to start
    Fixed KeyError for 'muted'
    on load it starts the loop
    on unload it stops the the loop

--- cogs/mod_tasks.py
+++ cogs/mod_tasks.py
@@ -8,18 +8,22 @@ from time import time
 class moderation_tasks(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.checkMuteTimer.start()
+
+    def cog_unload(self):
+        self.checkMuteTimer.cancel()
         
-    @tasks.loop(minutes=15)
+    @tasks.loop(minutes=10)
     async def checkMuteTimer(self):
         with open("records.json") as f:
             data = json.load(f)
         for user in data:
-            if user['timer'] < time() and user['isMute'] == 1:
+            if user['timer'] < time() and user['isMuted'] == 1:
                 user['timer'] = 0
-                user['isMute'] = 0
+                user['isMuted'] = 0
                 user['muteReason'] = "None"
                 guild = self.bot.get_guild(513196299613503488) # Hard coded zer0 server id
-                mutedRole = discord.utils.get(guild.roles, id = config.role_dict.get("muted"))
+                mutedRole = discord.utils.get(guild.roles, id = config.role_dict.get("Muted"))
                 member = guild.get_member(user['id'])
                 await member.remove_roles(mutedRole)
                 
